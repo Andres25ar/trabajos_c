5a17220444158f1e44b1d28c83dd39c15ce06081
Andrés Arancibia <106404047+Andres25ar@users.noreply.github.com>
2023-09-27 08:56:42 -0300

    Add files via upload

---
+++ COMPLETAR_EJ_BUSQ_SEC.c
@@ -0,0 +1,74 @@
+/* Problema 1: Dada una lista con las edades de N estudiantes y un número natural X, 
+ingresado por teclado, se solicita informar si dicho número se encuentra en la 
+lista ingresada y en qué posición ( Suponer que la lista A, NO está ordenada)*/
+
+#include <stdio.h>
+#define tamMax 30
+typedef int Tlista[tamMax];       /* Definición tipo de dato arreglo */
+
+/* ´PROTOTIPOS */
+void cargarLis(Tlista,int *);    /*Carga lista de edades*/
+int buscaEdad(Tlista,int,int);  /* Busca un nro en una lista NO ordenada*/
+
+int main(int argc, char *argv[]) {
+	Tlista edades;  /* edades: variable de Tipo de Dato Arreglo */
+	int N,X,bus;
+	
+	cargarLis(edades,&N);	
+	printf("\nIngrese la edad a buscar: ");
+	scanf("%d",&X);
+	bus=buscaEdad(edades, N, bus);
+	if(bus>0)
+		printf("\nEl elemento %d esta en la lista", X);
+	else
+		printf("\nEl elemento %d esta en la lista", X);
+	return 0;
+}
+
+/****** DEFINICION O IMPLEMENTACION DE MODULOS *****/
+void cargarLis(Tlista v,int *n1){	
+	int i;
+	do{
+		printf("\n Ingrese tamaño de lista: ");
+		scanf("%d",n1);
+	}while(*n1>=tamMax);
+	
+	for(i=1;i<=*n1;i++){
+		printf("\n ingrese elemento %d: ",i);
+		scanf("%d",&v[i]);
+	}	
+}
+		
+int buscaEdad(Tlista e,int n1,int eb){
+	int j, rta;
+	j=1;
+	while(e[j]!=eb&&j<=n1)
+		j++;
+	if(j<=n1)
+		rta=j;
+	else 
+		rta=-1;
+	return rta;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ COMPLETAR_EJ_ORDENAR.c
@@ -0,0 +1,62 @@
+-/* Problema 2: Dada una lista con las edades de N estudiantes y un número 
+natural X, ingresado por teclado, se solicita ordenar la lista y luego informar
+si dicho número se encuentra en la lista A ingresada.
+*/
+#include <stdio.h>
+typedef int Tlista[30];           /* Definición tipo de dato arreglo */
+
+/* ´PROTOTIPOS */
+void cargarLis(Tlista,int *);    /*Carga lista de edades*/
+void ordenaLis(Tlista,int);      /* Ordena lista */
+int busqBinaria(Tlista,int,int); /* Busca un nro en una lista ordenada*/
+
+int main(int argc, char *argv[]) {
+	Tlista edades;             /* edades: variable de Tipo de Dato Arreglo */
+	int N,X,pos;
+	
+	cargarLis(edades,&N);	
+	ordenaLis(........);
+	printf("\nIngrese la edad a buscar: ");
+	scanf("%d",&X);
+	pos=busqBinaria(........);
+	if(pos!=-1)
+	    printf("\nLa edad ingresada se encuentra el la posición %d", pos);
+	else
+		printf("\nNo se encontró la edad ingresada");
+			
+	return 0;
+}
+
+/****** DEFINICION O IMPLEMENTACION DE MODULOS *****/
+
+void cargarLis(Tlista v,int *n1){
+	
+	
+	/** COMPLETAR **/
+}
+
+void ordenaLis(Tlista v,int n1){
+	/* ...COMPLETAR ...*/
+}
+
+
+int busqBinaria(Tlista v,int n1,int b){
+	int ini,fin,med, ret;
+		
+	ini=1;fin=n1;med=(ini+fin)/2;
+		
+	while(ini<=fin && v[med]!=b){
+			if(v[med]<b)
+				ini=med+1;
+			else fin=med-1;
+			med=(ini+fin)/2;
+	}
+	if(ini<=fin) ret= med;
+	else ret -1;	
+	return ret;
+}
+		
+		
+ 
+
+
